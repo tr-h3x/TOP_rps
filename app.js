6a1f71937e93282dac3c0931bfd44578f3e5f9d5
tr_h3x <travis.hetzel@gmail.com>
2022-04-26 14:12:30 -0700

    Create function playRound
    Use switch statement to create function and test.

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ function computerPlay() {
 
 //console.log(computerPlay());
 
+
+const computerSelection = computerPlay();
+const playerSelection = "Scissors"
+//console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection + computerSelection) {
+        case "PaperRock":
+        case "ScissorsPaper":
+        case "RockScissors":
+            result = ("Player wins");
+            break;
+        case "RockPaper":
+        case "PaperScissors":
+        case "ScissorsRock":
+            result = ("Player lost!");
+            break;
+        case "PaperPaper":
+        case "RockRock":
+        case "ScissorsScissors": 
+            result = ("Draw!");
+            break;
+    }
+    return result;
+    
+}
+
+playRound(playerSelection, computerSelection);
+console.log(result);
+
+
+
+
+
+
